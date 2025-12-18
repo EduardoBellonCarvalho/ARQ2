370f0be2a56692075819daf95fe0bd23806d0b29
Eduardo Carvalho <140848952+EduardoBellonCarvalho@users.noreply.github.com>
2025-12-18 02:20:48 -0300

    Update riscv.asm

--- riscv.asm
+++ riscv.asm
@@ -1,15 +1,23 @@
 .text	
 .globl _start
 _start:
+
   addi t0,x0,1
   addi t1,x0,0
   addi t2,x0,15
+  li t3, 0x120 
+  li t4, 0x200
+
+check_pause:
+  lw t5, 0(t3)
+  and t5,t5,t4
+  bnez t5, check_pause
 
 loop:
   add t1,t0,t1
   addi t0,t0,1
   beq t0,t2,fim
-  j loop
+  j check_pause
 
 fim:
   ebreak
